f8d9f733a7ea0d28b438caf1efb24f8670d1917b
Alessio Colucci <alessio.colucci@gmail.com>
2019-03-08 19:48:58 +0100

    Added ImmediateGenerator,  all opcode definitions and basic interface for CU

--- cu/src/cu.sv
+++ cu/src/cu.sv
@@ -1,20 +1,35 @@
-package rv32i;
-    aaa
-endpackage
+`include "../../common/src/rv32i_defs.sv"
 
 // this cu is only combinational since the memory part is supplied by the pipeline registers
 // it simply takes all the codes from the current instruction and computes the corresponding output control signals
-module cu (opcode, funct3, funct7, controls);
-    output [] controls[]; // to be defined, first [] is the dimension of each element, second is number of elements
-    input [] opcode;
-    input [] funct3;
-    input [] funct7;
+module cu
+(
+    input [`INST_WIDTH-1:0] instruction,
+    output [`IMMEDIATE_SELECTION_WIDTH-1:0] imm_type,
+    output if_enable, id_reg, ex_reg, mem_reg, wb_reg,
+    output pc_load,
+    output inst_mem_enable,
+    output data_mem_write, data_mem_read,
+    output branch, jump,
+    output reg_file_write,
+    output mem2reg,
+    output MUX_selection_for_reg_dest // ???????????????
+);
+
 
     import rv32i;
 
     always_comb begin
-        case(opcode)
-            aa
+        case(instruction[0+:7])
+            RV32I_LUI_OPCODE :
+            begin
+            end
+
+            begin
+            end
+
+            default :
+
         endcase
     end
 endmodule
---
+++ immgen/src/immgen.sv
@@ -0,0 +1,59 @@
+`include "../../common/src/rv32i_defs.sv"
+
+module immgen
+(
+    input [`INST_WIDTH-1:0] instruction,
+    input [`IMMEDIATE_SELECTION_WIDTH-1:0] imm_type, // we need the CU to tell us the type of instruction
+    output [`WORD_WIDTH-1:0] immediate
+);
+
+    always_comb begin
+        case(imm_type)
+            I_TYPE :
+            begin
+                immediate[0] = instruction[20];
+                immediate[1+:4] = instruction[21+:4];
+                immediate[5+:6] = instruction[25+:6];
+                for (int i = 0; i < 22; i++)
+                    immediate[11+i] = instruction[31];
+            end
+            S_TYPE :
+            begin
+                immediate[0] = instruction[7];
+                immediate[1+:4] = instruction[8+:4];
+                immediate[5+:6] = instruction[25+:6];
+                for (int i = 0; i < 22; i++)
+                    immediate[11+i] = instruction[31];
+            end
+            B_TYPE :
+            begin
+                immediate[0] = 1'b0;
+                immediate[1+:4] = instruction[8+:4];
+                immediate[5+:6] = instruction[25+:6];
+                immediate[11] = instruction[7];
+                for (int i = 0; i < 22; i++)
+                    immediate[12+i] = instruction[31];
+            end
+            U_TYPE :
+            begin
+                for (int i = 0; i < 12; i++)
+                        immediate[i] = 0;
+                immediate[12+:8] = instruction[12+:8];
+                immediate[20+:11] = instruction[20+:11];
+                immediate[31] = instruction[31];
+            end
+            J_TYPE :
+            begin
+                immediate[0] = 1'b0;
+                immediate[1+:4] = instruction[21+:4];
+                immediate[5+:6] = instruction[25+:6];
+                immediate[11] = instruction[20];
+                immediate[12+:8] = instruction[12+:8];
+                for (int i = 0; i < 12; i++)
+                    immediate[20+i] = instruction[31];
+            end
+
+            default: immediate = 0;
+        endcase
+    end
+endmodule
