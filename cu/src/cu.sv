a07d73dd1052230b4629f0b828bdc1711d09ab5f
mp-17 <twisted@live.it>
2019-03-13 16:06:30 +0100

    dp schematic, to be refined

--- cu/src/cu.sv
+++ cu/src/cu.sv
@@ -7,7 +7,7 @@ module cu
     input [`INST_WIDTH-1:0] instruction,  // input instruction to be decoded
     output [`IMMEDIATE_SELECTION_WIDTH-1:0] imm_type, // to control the immediate generation unit 
     output D_MEM_write, D_MEM_read, D_MEM_mode, // control signal for the DMEM. "mode" is for bit width selection. #Old: MemWrite, MemRead, -
-    output RF_write, // write control signal for the RF. #Old: RegWrite
+    output RF_write, RF_regDest, // write control signal for the RF. #Old: RegWrite
     output RS2_IMM_ALU_SRC_MUX_sel, // sel for the mux on the second input (b) of the ALU. Select either IMM or $(Rs2). #Old: ALUSrc
     output DMEM_ALU_WB_MUX_sel, // sel for the mux in WB stage. Let pass either DMEM_dataOut or ALU_out. #Old: MemtoReg
     output branch, jump, jalr // asserted if the instruction is respectivly a branch, a JAL, a JALR. #OLD: Branch
